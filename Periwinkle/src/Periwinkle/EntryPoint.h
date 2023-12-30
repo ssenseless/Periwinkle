f6379624bbb1ab0e623e59990bcdc2741691988a
ssenseless <major.add.9@ou.edu>
2023-12-30 01:17:18 -0800

    unresolved external bullshit I'll deal with at not 3 am

--- Periwinkle/src/Periwinkle/EntryPoint.h
+++ Periwinkle/src/Periwinkle/EntryPoint.h
@@ -5,6 +5,18 @@
 extern Periwinkle::Application* Periwinkle::CreateApplication();
 
 int main(int argc, char** argv) {
+	Periwinkle::Log::init();
+
+	PW_CORE_TRACE("HELP");
+	PW_CORE_INFO("HELP");
+	PW_CORE_WARN("HELP");
+	PW_CORE_ERROR("HELP");
+
+	PW_TRACE("HELP");
+	PW_INFO("HELP");
+	PW_WARN("HELP");
+	PW_ERROR("HELP");
+
 	auto app = Periwinkle::CreateApplication();
 	
 	app->Run();
